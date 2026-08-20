54141324444468c4b6be212699be6ffe499ef6f0
Daniel Souza <daniel.lucas@braskem.com>
2026-08-20 09:48:05 -0300

    refactor: adicionando force true no ecr repository

--- tech-challenge-backend/terraform/ecr.tf
+++ tech-challenge-backend/terraform/ecr.tf
@@ -2,6 +2,8 @@ resource "aws_ecr_repository" "app" {
   name                 = "tech-challenge-backend"
   image_tag_mutability = "MUTABLE"
 
+  force_delete = true
+
   image_scanning_configuration {
     scan_on_push = true
   }
