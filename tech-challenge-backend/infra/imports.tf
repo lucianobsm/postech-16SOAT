3b75fea1177060e36dddd662c3267d011858b9b6
Daniel Souza <daniel.lucas@braskem.com>
2026-08-31 00:12:06 -0300

    fix: adding only bucket import

--- tech-challenge-backend/infra/imports.tf
+++ tech-challenge-backend/infra/imports.tf
@@ -1,9 +1,4 @@
 import {
-  to = aws_ecr_repository.app
-  id = "tech-challenge-backend"
-}
-
-import {
   to = module.state_bucket.aws_s3_bucket.terraform_state
   id = "tech-challenge-soat16-gp15-daniel"   # o nome que você decidiu manter
 }
